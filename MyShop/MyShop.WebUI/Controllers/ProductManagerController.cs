c8b1d1e02eb09d86a8c5e282dc88b06decfe90ea
Laser-Sword <38383555+laser-sword@users.noreply.github.com>
2020-08-30 15:38:43 +0100

    Images uploadedable

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -60,12 +61,16 @@ namespace MyShop.WebUI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product) {
+        public ActionResult Create(Product product, HttpPostedFileBase file) {
             if (!ModelState.IsValid)
             {
                 return View(product);
             }
             else {
+                if (file != null) {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
                 context.Insert(product);
                 context.Commit();
 
@@ -93,7 +98,7 @@ namespace MyShop.WebUI.Controllers
             
         }
         [HttpPost]
-        public ActionResult Edit(Product product, string Id) {
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file) {
             Product productToEdit = context.Find(Id);
 
             if (productToEdit == null)
@@ -104,10 +109,14 @@ namespace MyShop.WebUI.Controllers
                 if (!ModelState.IsValid) {
                     return View(product);
                 }
+                if (file != null) {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
+                }
                 //if all ok pass through the info to all tyhe properties of the product
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
+                //productToEdit.Image = product.Image;
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
 
