1abb080246ccbe0c563ef0d69a78d16ac690b2a1
Laser-Sword <38383555+laser-sword@users.noreply.github.com>
2020-09-05 14:38:00 +0100

    Added index page for product listings

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,42 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+
+        //ProductCategoryRepository productCategories;
+        //then create a contructor to initialize thats product repositry
+        //so you can send through the product and list of product categories
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            //context = new ProductRepository();
+            ////initialize the repository
+            //productCategories = new ProductCategoryRepository();
+
+
+            //context = new InMemoryRepository<Product>();
+            //productCategories = new InMemoryRepository<ProductCategory>();
+            context = productContext;
+            productCategories = productCategoryContext;
+
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
         }
+        public ActionResult Details(string Id) {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else {
+                return View(product);
+            }
+        } 
 
         public ActionResult About()
         {
